22dcabe19372b25b03c25af217f297626c320c33
minhazf456 <minhazf@gmail.com>
2020-03-10 22:41:57 -0700

    setting up Redux into the application

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -38,4 +38,8 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect (mapStateToProps)(Header);
